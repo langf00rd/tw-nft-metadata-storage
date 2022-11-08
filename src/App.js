e3b00ffe9136963c5adbc3f77c12dd4e029fc7d0
langford <langfordquarshie21@gmail.com>
2022-11-08 22:22:07 +0000

    🔥🔥🔥

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
 import './App.css';
 
+const storage = new ThirdwebStorage();
+
 function App() {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [image, setImage] = useState('')
+  const [link, setLink] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const uploadMetadata = async () => {
+    setLoading(true)
+    const metadata = {
+      name: name,
+      description: description,
+      image: image
+    }
+
+    const uri = await storage.upload(metadata)
+    const url = storage.resolveScheme(uri)
+
+    setLink(url)
+    setLoading(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>NFT metadata upload</h1>
+      <input placeholder="name" onChange={e => setName(e.target.value)} />
+      <textarea placeholder="description" onChange={e => setDescription(e.target.value)} />
+      <input placeholder="image" onChange={e => setImage(e.target.value)} />
+
+      {
+        loading
+          ? <p>uploading...</p>
+          : <button onClick={uploadMetadata}>upload metadata 🚀</button>
+      }
+
+      <a target='blank' href={link} className="link">{link}</a>
+    </main>
+  )
 }
 
 export default App;
